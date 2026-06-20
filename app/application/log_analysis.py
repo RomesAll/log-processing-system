fcee192fbedff271e023c9d71a83e991ea9db1d3
RomesAll <romesky845@gmail.com>
2026-06-20 20:21:39 +0300

    feat(app): добавить формирование локальной статистике воркерами для порции данных и передачи их в следующую очередь для финального сбора статистики

--- app/application/log_analysis.py
+++ app/application/log_analysis.py
@@ -1,5 +1,12 @@
+import os
+
 from app.application.base import PipelineStage
 from multiprocessing import Queue, Process
+from collections import Counter
+
+from app.domain_models.log_entry import LogEntry
+from app.domain_models.partial_analysis_result import PartialAnalysisResult
+
 
 class LogAnalyzerStage(PipelineStage):
     def __init__(self,
@@ -11,3 +18,54 @@ class LogAnalyzerStage(PipelineStage):
         self.output_queue = output_queue
         self.num_workers = num_workers
         self._workers: list[Process] = []
+
+    def start(self):
+        for i in range(self.num_workers):
+            p = Process(
+                target=self._analyzer_worker,
+                args=(i,),
+                name=f'{self.name}_{i}'
+            )
+            p.start()
+            self._workers.append(p)
+
+    def _analyzer_worker(self):
+        pid = os.getpid()
+        print(f'{self.name} pid: {pid} запущен')
+        while True:
+            entries: list[LogEntry] = self.input_queue.get()
+            if entries is None:
+                print(f'{self.name} pid: {pid} получил стоп сигнал, обработано чанков: {processed_chunk}')
+                self.output_queue.put(None)
+                break
+            counter_status = Counter()
+            counter_url = Counter()
+            referer = Counter()
+            error_ips = set()
+            count_entries = 0
+            total_size = 0
+            sample_entries = entries[:5]
+            for entry in entries:
+                counter_status[entry.status_code] += 1
+                counter_url[entry.url] += 1
+                referer[entry.referer] += 1
+                count_entries += 1
+                total_size += entry.size
+
+                if entry.status_code == "500":
+                    error_ips.add(entry.ip)
+
+            result = PartialAnalysisResult(
+                count_entries=count_entries,
+                counter_url=counter_url,
+                counter_status=counter_status,
+                total_size=total_size,
+                error_ips=error_ips,
+                sample_entries=sample_entries,
+            )
+            self.output_queue.put(result)
+
+
+    def join(self, timeout=None):
+        for worker in self._workers:
+            worker.join(timeout)
