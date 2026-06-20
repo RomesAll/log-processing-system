366d85953cd10dadebc2fe62fb9dccfdd6285b5f
RomesAll <romesky845@gmail.com>
2026-06-20 17:16:47 +0300

    feat(app): добавить метод для чтения из файла и доб. логово в очередь дв новом процессе

--- app/application/log_reader.py
+++ app/application/log_reader.py
@@ -1,5 +1,5 @@
 from app.application.base import PipelineStage
-from multiprocessing import Queue
+from multiprocessing import Queue, Process
 import os
 
 class LogReaderStage(PipelineStage):
@@ -12,3 +12,26 @@ class LogReaderStage(PipelineStage):
         self.chunk_size = chunk_size
         self.num_consumer = num_consumer or os.cpu_count()
 
+    def start(self):
+        self.process = Process(target=self._read_file, name=self.name)
+        self.process.start()
+
+    def _read_file(self):
+        print(f"[{self.name}] запущен в процессе {os.getpid()}")
+        try:
+            with open(self.filepath) as file:
+                chunk = []
+                for line in file:
+                    chunk.append(line)
+                    if len(chunk) >= self.chunk_size:
+                        self.output_queue.put(chunk)
+                        chunk = []
+                if chunk:
+                    self.output_queue.put(chunk)
+        except FileNotFoundError:
+            print(f"[ERROR] {self.name}]: файл не найден - {self.filepath}")
+        finally:
+            for _ in range(self.count_consumer):
+                self.output_queue.put(None)
+            print(f"[OK] обработка в процессе {os.getpid()} завершена, "
+                  f"передано {self.count_consumer} стоп-сигналов")
