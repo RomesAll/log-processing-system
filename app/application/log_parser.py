d909b257a6a86c3b959047b0db15ff1e34d1a011
RomesAll <romesky845@gmail.com>
2026-06-20 19:04:56 +0300

    feat(app): добавить метод для парсинга сырых строк из логов и преобразования их в доменные модели и добавление в следующую очередь (для сбора статистики)

--- app/application/log_parser.py
+++ app/application/log_parser.py
@@ -1,4 +1,5 @@
 from app.application.base import PipelineStage
+from app.domain_models.log_entry import LogEntry
 from multiprocessing import Queue, Process
 import os, re
 
@@ -14,3 +15,35 @@ class LogParserStage(PipelineStage):
         self.num_workers = num_workers or os.cpu_count()
         self._workers: list[Process] = []
         self.parse_pattern = parse_pattern
+
+    def start(self):
+        for i in range(self.num_workers):
+            p = Process(target=self._parser_worker, args=(i,), name=f'{self.name}_{i}')
+            p.start()
+            self._workers.append(p)
+
+    def _parser_worker(self, worker_id: int):
+        pid = os.getpid()
+        processed_chunk: int = 0
+        print(f'{self.name} pid: {pid} запущен')
+        while True:
+            row_chunk = self.input_queue.get()
+            if row_chunk is None:
+                print(f'{self.name} pid: {pid} получил стоп сигнал, обработано чанков: {processed_chunk}')
+                self.output_queue.put(None)
+                break
+            parsed_entries = []
+            for chunk in row_chunk:
+                entry = LogEntry.from_line(
+                    chunk,
+                    self.parse_pattern,
+                )
+                if entry:
+                    parsed_entries.append(entry)
+            if parsed_entries:
+                processed_chunk += 1
+                self.output_queue.put(parsed_entries)
+
+    def join(self, timeout=None):
+        for p in self._workers:
+            p.join(timeout)
