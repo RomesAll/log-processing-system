8a394d79765915548e0404085b9b5967a35fef9f
RomesAll <romesky845@gmail.com>
2026-06-20 15:20:02 +0300

    feat(domain models): добавить класс PartialAnalysisResult
    - Добавленный класс нужен для хранения промежуточных результатов каждого рабочего процесса для своей группы данных (строк из логов)

---
+++ domain_models/partial_analysis_result.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from domain_models.log_entry import LogEntry
+
+@dataclass
+class PartialAnalysisResult:
+    count_entries: int = field(default=0)
+    counter_url: dict[str, int] = field(default_factory=dict)
+    counter_status: dict[str, int] = field(default_factory=dict)
+    total_size: int = field(default=0)
+    error_ips: list[str] = field(default_factory=list)
+    sample_entries: list['LogEntry'] = field(default_factory=list)
