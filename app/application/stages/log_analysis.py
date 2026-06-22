6540c80b5debae4d512d002d03bf69d90d498fda
RomesAll <romesky845@gmail.com>
2026-06-22 13:04:23 +0300

    feat(app): реализовать метод (_target) для анализа распарсенных значений и подготовки промежуточной статистики

--- app/application/stages/log_analysis.py
+++ app/application/stages/log_analysis.py
@@ -1,59 +1,46 @@
-import os
-
-from app.application.base import PipelineStage
-from multiprocessing import Queue, Process
+from app.application.stages.base import PipelineStage
+from multiprocessing import Queue
 from collections import Counter
 
 from app.domain_models.log_entry import LogEntry
 from app.domain_models.partial_analysis_result import PartialAnalysisResult
-
+import os
 
 class LogAnalyzerStage(PipelineStage):
     def __init__(self,
                  input_queue: Queue,
                  output_queue: Queue,
-                 num_workers: int | None = None):
-        super().__init__('LogAnalyzer')
+                 worker_process: int | None = None):
+        super().__init__('LogAnalyzer', worker_process)
         self.input_queue = input_queue
         self.output_queue = output_queue
-        self.num_workers = num_workers
-        self._workers: list[Process] = []
 
-    def start(self):
-        for i in range(self.num_workers):
-            p = Process(
-                target=self._analyzer_worker,
-                args=(i,),
-                name=f'{self.name}_{i}'
-            )
-            p.start()
-            self._workers.append(p)
-
-    def _analyzer_worker(self):
+    def _target(self):
         pid = os.getpid()
-        print(f'{self.name} pid: {pid} запущен')
         while True:
             entries: list[LogEntry] = self.input_queue.get()
             if entries is None:
-                print(f'{self.name} pid: {pid} получил стоп сигнал, обработано чанков: {processed_chunk}')
                 self.output_queue.put(None)
                 break
+            counter_ip = Counter()
             counter_status = Counter()
             counter_url = Counter()
-            referer = Counter()
+            counter_referer = Counter()
+            counter_method = Counter()
             error_ips = set()
             count_entries = 0
             total_size = 0
-            sample_entries = entries[:5]
             for entry in entries:
                 counter_status[entry.status_code] += 1
                 counter_url[entry.url] += 1
-                referer[entry.referer] += 1
+                counter_referer[entry.referer] += 1
+                counter_ip[entry.ipv4] += 1
+                counter_method[entry.http_method] += 1
                 count_entries += 1
-                total_size += entry.size
+                total_size += int(entry.size_byte)
 
                 if entry.status_code == "500":
-                    error_ips.add(entry.ip)
+                    error_ips.add(entry.ipv4)
 
             result = PartialAnalysisResult(
                 count_entries=count_entries,
@@ -61,11 +48,8 @@ class LogAnalyzerStage(PipelineStage):
                 counter_status=counter_status,
                 total_size=total_size,
                 error_ips=error_ips,
-                sample_entries=sample_entries,
+                counter_ip=counter_ip,
+                counter_referer=counter_referer,
+                counter_method=counter_method,
             )
             self.output_queue.put(result)
-
-
-    def join(self, timeout=None):
-        for worker in self._workers:
-            worker.join(timeout)
