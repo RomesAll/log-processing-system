bebaf03cb1de4c86a6912ee6d39c1fc6e1fc15fa
RomesAll <romesky845@gmail.com>
2026-06-22 13:10:51 +0300

    feat(domain): добавить атрибут для хранения кол-ва referer

--- app/domain_models/final_analysis_result.py
+++ app/domain_models/final_analysis_result.py
@@ -6,6 +6,7 @@ class FinalAnalysisResult(BaseModel):
     total_entries: int = Field(default=0, ge=0)
     total_methods: dict[str, int] = Field(default_factory=dict)
     total_status: dict[str, int] = Field(default_factory=dict)
+    total_referer: dict[str, int] = Field(default_factory=dict)
     top_urls: dict[str, int] = Field(default_factory=dict)
     top_ips: dict[str, int] = Field(default_factory=dict)
     suspicious_ips_error: set = Field(default_factory=set)
@@ -20,6 +21,7 @@ class FinalAnalysisResult(BaseModel):
                 'total_entries': self.total_entries,
                 'processing_time_seconds': self.processing_time_seconds,
             },
+            'total_referer': self.total_referer,
             'total_methods': self.total_methods,
             'total_status': self.total_status,
             'top_urls': self.top_urls,
