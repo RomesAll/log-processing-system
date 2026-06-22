ca2933c63438d7318e0c0154e9eca64ccd130bbd
RomesAll <romesky845@gmail.com>
2026-06-22 13:11:18 +0300

    feat(domain): добавить атрибут counter_referer для хранения кол-во referer

--- app/domain_models/partial_analysis_result.py
+++ app/domain_models/partial_analysis_result.py
@@ -6,5 +6,6 @@ class PartialAnalysisResult(BaseModel):
     counter_status: dict[str, int] = Field(default_factory=dict)
     counter_ip: dict[str, int] = Field(default_factory=dict)
     counter_method: dict[str, int] = Field(default_factory=dict)
+    counter_referer: dict[str, int] = Field(default_factory=dict)
     total_size: int = Field(default=0, ge=0)
     error_ips: set = Field(default_factory=set)
